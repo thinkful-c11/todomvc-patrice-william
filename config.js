bc8f352fa9279f286e024df188462680c283ad87
Chris Klanac <cklanac@gmail.com>
2017-06-01 07:15:38 -0400

    add dotenv

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 'use strict';
 
 require('dotenv').config();
-
-const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://dev:dev@localhost/todos-app';
+const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL;
 
 exports.DATABASE = {
   client: 'pg',
   connection: DATABASE_URL,
+  pool: { min: 0, max: 3 },
   // debug: true
 };
 
