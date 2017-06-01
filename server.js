a852033b561be71a0e8da57dfdc89d9a4265b70f
williamtwobit <william@twobit.cc>
2017-06-01 12:01:18 -0400

    skeleton endpoints passing tests

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const { DATABASE, PORT } = require('./config');
 
 const app = express();
 
 // Add middleware and .get, .post, .put and .delete endpoints
 
-app.get('/',(req,res)=>{
-  res.send('Hello World');
+// CORS middleware //
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  next();
 });
 
+// Body Parser middleware //
+app.use(bodyParser.json());
+
+///// GET and POST endpoint skeleton //////
+
+// should respond to GET with status 200 and an array
+app.get('/api/items',(req,res)=>{
+  res.json([]).sendStatus(200);
+});
+
+
+
+app.post('/api/items',(req,res)=>{
+
+  //should respond to POST with an object a status 201 and a location header
+  //should respond to POST with the title of item that was POSTed
+
+  res.location('placeholder').status(201).json({title: req.body.title});
+
+});
 
 let server;
 let knex;
